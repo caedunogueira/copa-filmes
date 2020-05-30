d01c34046fed198f83c4db3c2463885d36f73f87
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-05-30 18:19:40 -0300

    adicionado mais cenários de testes para filmes com notas iguais em que o desempate se dará pelo titulo.

--- CopaFilmes.Tests/Domain/Implementacoes/PartidaTests.cs
+++ CopaFilmes.Tests/Domain/Implementacoes/PartidaTests.cs
@@ -36,15 +36,47 @@ namespace CopaFilmes.Tests.Domain.Implementacoes
         }
 
         [TestMethod]
-        public void PartidaTests_Dado_Dois_Filmes_Com_Notas_Iguais_Quando_Disputarem_Uma_Partida_Retorna_Como_Vencedor_O_Filme_Em_Primeiro_Na_Ordem_Alfabetica_De_Titulo()
+        [DynamicData(nameof(FilmesComNotasIguais), DynamicDataSourceType.Property)]
+        public void PartidaTests_Dado_Dois_Filmes_Com_Notas_Iguais_Quando_Disputarem_Uma_Partida_Retorna_Como_Vencedor_O_Filme_Em_Primeiro_Na_Ordem_Alfabetica_De_Titulo(Filme filmeA, Filme filmeB, int posicaoVencedor)
         {
-            var filmeA = new FilmeTestBuilder().ComTitulo("Filme A").ComNota(8.5m).Build();
-            var filmeB = new FilmeTestBuilder().ComTitulo("Filme B").ComNota(8.5m).Build();
             var partida = new Partida(filmeA, filmeB);
+            var vencedorEsperado = posicaoVencedor == 1 ? filmeA : filmeB;
 
             partida.Disputar();
 
-            Assert.AreEqual(expected: filmeA, actual: partida.Vencedor);
+            Assert.AreEqual(expected: vencedorEsperado, actual: partida.Vencedor);
+        }
+
+        public static IEnumerable<object[]> FilmesComNotasIguais
+        {
+            get
+            {
+                yield return new object[] { 
+                    new FilmeTestBuilder().ComTitulo("Filme A").ComNota(8.5m).Build(), 
+                    new FilmeTestBuilder().ComTitulo("Filme B").ComNota(8.5m).Build(), 
+                    1 
+                };
+                yield return new object[] { 
+                    new FilmeTestBuilder().ComTitulo("Deadpool 2").ComNota(8.5m).Build(), 
+                    new FilmeTestBuilder().ComTitulo("Vingadores: Guerra Finita").ComNota(8.5m).Build(), 
+                    1 
+                };
+                yield return new object[] { 
+                    new FilmeTestBuilder().ComTitulo("Han Solo: Uma História Star Wars").ComNota(8.5m).Build(), 
+                    new FilmeTestBuilder().ComTitulo("Thor: Ragnarok").ComNota(8.5m).Build(), 
+                    1 
+                };
+                yield return new object[] { 
+                    new FilmeTestBuilder().ComTitulo("Hereditário").ComNota(8.5m).Build(), 
+                    new FilmeTestBuilder().ComTitulo("Os Incríveis 2").ComNota(8.5m).Build(), 
+                    1 
+                };
+                yield return new object[] { 
+                    new FilmeTestBuilder().ComTitulo("Jurassic World: Reino Ameaçado").ComNota(8.5m).Build(), 
+                    new FilmeTestBuilder().ComTitulo("Oito Mulheres e um Segredo").ComNota(8.5m).Build(), 
+                    1
+                };
+            }
         }
     }
 }
