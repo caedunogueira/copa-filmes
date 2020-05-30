332c9b59a8a0912f8a209db04d67e75f567be2fe
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-05-30 13:10:57 -0300

    adicionado teste para quando um filme possuir nota maior que o outro.

--- CopaFilmes.Tests/Domain/Implementacoes/FilmeTests.cs
+++ CopaFilmes.Tests/Domain/Implementacoes/FilmeTests.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using CopaFilmes.WebAPI.Domain.Implementacoes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CopaFilmes.Tests.Domain.Implementacoes
@@ -6,6 +6,13 @@ namespace CopaFilmes.Tests.Domain.Implementacoes
     [TestClass]
     public class FilmeTests
     {
+        [TestMethod]
+        public void FilmeTests_Dado_Que_Filme_Possui_Maior_Nota_Em_Relacao_Ao_Comparado_Quando_Consultar_Retorna_Verdadeiro()
+        {
+            var filmeA = new Filme(id: "tt1245748", titulo: "Filme A", ano: 2018, nota: 8.5m);
+            var filmeB = new Filme(id: "tt2532658", titulo: "Filme B", ano: 2019, nota: 6.7m);
 
+            Assert.IsTrue(filmeA.PossuiMaiorNota(filmeB));
+        }
     }
 }
---
+++ CopaFilmes.WebAPI/Domain/Implementacoes/Filme.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace CopaFilmes.WebAPI.Domain.Implementacoes
+{
+    public class Filme
+    {
+        readonly string _id, _titulo;
+        readonly int _ano;
+        readonly decimal _nota;
+
+        public Filme(string id, string titulo, int ano, decimal nota)
+        {
+            _id = id;
+            _titulo = titulo;
+            _ano = ano;
+            _nota = nota;
+        }
+
+        public bool PossuiMaiorNota(Filme outroFilme)
+        {
+            return true;
+        }
+    }
+}
