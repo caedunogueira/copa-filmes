d00ff06a86284984bccbf1f05a496afd68c669d7
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-05-30 13:21:56 -0300

    adicionado teste de comparação de nota quando os filmes possuirem notas iguais.

--- CopaFilmes.Tests/Domain/Implementacoes/FilmeTests.cs
+++ CopaFilmes.Tests/Domain/Implementacoes/FilmeTests.cs
@@ -23,5 +23,14 @@ namespace CopaFilmes.Tests.Domain.Implementacoes
 
             Assert.IsFalse(filmeB.PossuiMaiorNota(filmeA));
         }
+
+        [TestMethod]
+        public void FilmeTests_Dado_Que_Filme_Possui_Nota_Igual_Em_Relacao_Ao_Comparado_Quando_Consultar_Retorna_Falso()
+        {
+            var filmeA = new Filme(id: "tt1245748", titulo: "Filme A", ano: 2018, nota: 8.5m);
+            var filmeB = new Filme(id: "tt2532658", titulo: "Filme B", ano: 2019, nota: 8.5m);
+
+            Assert.IsFalse(filmeA.PossuiMaiorNota(filmeB));
+        }
     }
 }
