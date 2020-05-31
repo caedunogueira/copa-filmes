9d64c71608f16eb25867a12bb3e99ca72c3874d3
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-05-31 09:45:02 -0300

    alterado implementação para melhorar legibilidade do código e entendimento da regra.

--- CopaFilmes.WebAPI/Domain/Implementacoes/Partida.cs
+++ CopaFilmes.WebAPI/Domain/Implementacoes/Partida.cs
@@ -18,14 +18,14 @@ namespace CopaFilmes.WebAPI.Domain.Implementacoes
 
         internal void Disputar()
         {
-            if (!_primeiroFilme.PossuiNotaMaior(_segundoFilme) && !_segundoFilme.PossuiNotaMaior(_primeiroFilme))
-            {
-                Vencedor = new Filme[] { _primeiroFilme, _segundoFilme }.OrderBy(f => f)
-                                                                        .First();
-                return;
-            }
-
-            Vencedor = _primeiroFilme.PossuiNotaMaior(_segundoFilme) ? _primeiroFilme : _segundoFilme;
+            if (_primeiroFilme.PossuiNotaIgual(_segundoFilme))
+                Vencedor = new Filme[] { _primeiroFilme, _segundoFilme }
+                    .OrderBy(f => f)
+                    .First();
+            else
+                Vencedor = _primeiroFilme.PossuiNotaMaior(_segundoFilme) 
+                    ? _primeiroFilme 
+                    : _segundoFilme;
         }
     }
 }
