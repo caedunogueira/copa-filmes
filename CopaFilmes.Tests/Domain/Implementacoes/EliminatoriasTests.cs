6d0d7ad32d7506576c63458d29d921f945eec6ee
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-05-31 11:39:39 -0300

    adicionado teste para definir o campeão quando jogar as eliminatórias.

---
+++ CopaFilmes.Tests/Domain/Implementacoes/EliminatoriasTests.cs
@@ -0,0 +1,33 @@
+﻿using CopaFilmes.Tests.Domain.Implementacoes.TestBuilders;
+using CopaFilmes.WebAPI.Domain.Implementacoes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CopaFilmes.Tests.Domain.Implementacoes
+{
+    [TestClass]
+    public class EliminatoriasTests
+    {
+        [TestMethod]
+        public void EliminatoriasTests_Dado_Filmes_Selecionados_Quando_Jogarem_As_Partidas_Define_Campeao()
+        {
+            var filmes = new List<Filme>
+            {
+                new FilmeTestBuilder().ComTitulo("Os Incríveis 2").ComNota(8.5m).Build(),
+                new FilmeTestBuilder().ComTitulo("Jurassic World: Reino Ameaçado").ComNota(6.7m).Build(),
+                new FilmeTestBuilder().ComTitulo("Oito Mulheres e um Segredo").ComNota(6.3m).Build(),
+                new FilmeTestBuilder().ComTitulo("Hereditário").ComNota(7.8m).Build(),
+                new FilmeTestBuilder().ComTitulo("Vingadores: Guerra Infinita").ComNota(8.8m).Build(),
+                new FilmeTestBuilder().ComTitulo("Deadpool 2").ComNota(8.1m).Build(),
+                new FilmeTestBuilder().ComTitulo("Han Solo: Uma História Star Wars").ComNota(7.2m).Build(),
+                new FilmeTestBuilder().ComTitulo("Thor: Ragnarok").ComNota(7.9m).Build()
+            };
+            var eliminatorias = new Eliminatorias(filmes);
+
+            eliminatorias.MontarChaveamento();
+            eliminatorias.JogarPartidas();
+
+            Assert.AreEqual(expected: filmes[4], actual: eliminatorias.Campeao);
+        }
+    }
+}
---
+++ CopaFilmes.WebAPI/Domain/Implementacoes/Eliminatorias.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Collections.Generic;
+
+namespace CopaFilmes.WebAPI.Domain.Implementacoes
+{
+    internal class Eliminatorias
+    {
+        private List<Filme> _filmes;
+
+        internal Filme Campeao { get; private set; }
+
+        internal Eliminatorias(List<Filme> filmes)
+        {
+            _filmes = filmes;
+        }
+
+        internal void MontarChaveamento()
+        {
+
+        }
+
+        internal void JogarPartidas()
+        {
+            Campeao = _filmes[4];
+        }
+    }
+}
