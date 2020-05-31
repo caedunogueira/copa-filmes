3c5b3f96f15b04ea052e7b0acd43ac52c6aa9524
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-05-31 13:13:05 -0300

    adicionado cenário de teste para escolha de um campeão diferente.

--- CopaFilmes.Tests/Domain/Implementacoes/EliminatoriasTests.cs
+++ CopaFilmes.Tests/Domain/Implementacoes/EliminatoriasTests.cs
@@ -9,25 +9,52 @@ namespace CopaFilmes.Tests.Domain.Implementacoes
     public class EliminatoriasTests
     {
         [TestMethod]
-        public void EliminatoriasTests_Dado_Filmes_Selecionados_Quando_Jogarem_As_Partidas_Define_Campeao()
+        [DynamicData(nameof(FilmesSelecionadosParaEliminatorias), DynamicDataSourceType.Property)]
+        public void EliminatoriasTests_Dado_Filmes_Selecionados_Quando_Jogarem_As_Partidas_Define_Campeao(List<Filme> filmes, int indiceFilmeVencedor)
         {
-            var filmes = new List<Filme>
-            {
-                new FilmeTestBuilder().ComTitulo("Os Incríveis 2").ComNota(8.5m).Build(),
-                new FilmeTestBuilder().ComTitulo("Jurassic World: Reino Ameaçado").ComNota(6.7m).Build(),
-                new FilmeTestBuilder().ComTitulo("Oito Mulheres e um Segredo").ComNota(6.3m).Build(),
-                new FilmeTestBuilder().ComTitulo("Hereditário").ComNota(7.8m).Build(),
-                new FilmeTestBuilder().ComTitulo("Vingadores: Guerra Infinita").ComNota(8.8m).Build(),
-                new FilmeTestBuilder().ComTitulo("Deadpool 2").ComNota(8.1m).Build(),
-                new FilmeTestBuilder().ComTitulo("Han Solo: Uma História Star Wars").ComNota(7.2m).Build(),
-                new FilmeTestBuilder().ComTitulo("Thor: Ragnarok").ComNota(7.9m).Build()
-            };
             var eliminatorias = new Eliminatorias(filmes);
 
             eliminatorias.MontarChaveamento();
             eliminatorias.JogarPartidas();
 
-            Assert.AreEqual(expected: filmes[4], actual: eliminatorias.Campeao);
+            Assert.AreEqual(expected: filmes[indiceFilmeVencedor], actual: eliminatorias.Campeao);
+        }
+
+        public static IEnumerable<object[]> FilmesSelecionadosParaEliminatorias
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    new List<Filme>
+                    {
+                        new FilmeTestBuilder().ComTitulo("Os Incríveis 2").ComNota(8.5m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Jurassic World: Reino Ameaçado").ComNota(6.7m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Oito Mulheres e um Segredo").ComNota(6.3m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Hereditário").ComNota(7.8m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Vingadores: Guerra Infinita").ComNota(8.8m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Deadpool 2").ComNota(8.1m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Han Solo: Uma História Star Wars").ComNota(7.2m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Thor: Ragnarok").ComNota(7.9m).Build()
+                    },
+                    4
+                };
+                yield return new object[]
+                {
+                    new List<Filme>
+                    {
+                        new FilmeTestBuilder().ComTitulo("Jurassic World: Reino Ameaçado").ComNota(6.7m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Oito Mulheres e um Segredo").ComNota(6.3m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Hereditário").ComNota(7.8m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Han Solo: Uma História Star Wars").ComNota(7.2m).Build(),
+                        new FilmeTestBuilder().ComTitulo("A Barraca do Beijo").ComNota(6.4m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Tomb Raider: A Origem").ComNota(6.5m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Thor: Ragnarok").ComNota(7.9m).Build(),
+                        new FilmeTestBuilder().ComTitulo("Pantera Negra: A Origem").ComNota(7.5m).Build(),
+                    },
+                    6
+                };
+            }
         }
     }
 }
--- CopaFilmes.WebAPI/Domain/Implementacoes/Eliminatorias.cs
+++ CopaFilmes.WebAPI/Domain/Implementacoes/Eliminatorias.cs
@@ -1,11 +1,13 @@
 ﻿
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CopaFilmes.WebAPI.Domain.Implementacoes
 {
     internal class Eliminatorias
     {
         private List<Filme> _filmes;
+        private Partida[] _partidas;
 
         internal Filme Campeao { get; private set; }
 
@@ -16,12 +18,32 @@ namespace CopaFilmes.WebAPI.Domain.Implementacoes
 
         internal void MontarChaveamento()
         {
+            _partidas = new Partida[4];
+            
+            _filmes = _filmes.OrderBy(f => f).ToList();
 
+            _partidas[0] = new Partida(_filmes[0], _filmes[7]);
+            _partidas[1] = new Partida(_filmes[1], _filmes[6]);
+            _partidas[2] = new Partida(_filmes[2], _filmes[5]);
+            _partidas[3] = new Partida(_filmes[3], _filmes[4]);
         }
 
         internal void JogarPartidas()
         {
-            Campeao = _filmes[4];
+            foreach (var partida in _partidas)
+                partida.Disputar();
+
+            var novasPartidas = new Partida[2];
+            novasPartidas[0] = new Partida(_partidas[0].Vencedor, _partidas[1].Vencedor);
+            novasPartidas[1] = new Partida(_partidas[2].Vencedor, _partidas[3].Vencedor);
+
+            foreach (var partida in novasPartidas)
+                partida.Disputar();
+
+            var ultimaPartida = new Partida(novasPartidas[0].Vencedor, novasPartidas[1].Vencedor);
+            ultimaPartida.Disputar();
+
+            Campeao = ultimaPartida.Vencedor;
         }
     }
 }
