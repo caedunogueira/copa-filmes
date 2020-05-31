fff5ee0423aa3a9d246b4975895ff105e3b0bbf5
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-05-31 19:31:53 -0300

    adicionado teste para obter o campeão e vice-campeão da copa do mundo ao adicionar filmes para participar da copa.

---
+++ CopaFilmes.Tests/Domain/Implementacoes/CopaMundoTests.cs
@@ -0,0 +1,36 @@
+﻿using CopaFilmes.Tests.Domain.Implementacoes.TestBuilders;
+using CopaFilmes.WebAPI.Domain.Implementacoes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CopaFilmes.Tests.Domain.Implementacoes
+{
+    [TestClass]
+    public class CopaMundoTests
+    {
+        [TestMethod]
+        public void CopaMundoTests_Dado_Filmes_Selecionados_Quando_Jogar_A_Copa_Obtem_O_Campeao_E_Vice()
+        {
+            var copa = new CopaMundo();
+            var filmes = new List<Filme>
+            {
+                new FilmeTestBuilder().ComTitulo("Os Incríveis 2").ComNota(8.5m).Build(),
+                new FilmeTestBuilder().ComTitulo("Jurassic World: Reino Ameaçado").ComNota(6.7m).Build(),
+                new FilmeTestBuilder().ComTitulo("Oito Mulheres e um Segredo").ComNota(6.3m).Build(),
+                new FilmeTestBuilder().ComTitulo("Hereditário").ComNota(7.8m).Build(),
+                new FilmeTestBuilder().ComTitulo("Vingadores: Guerra Infinita").ComNota(8.8m).Build(),
+                new FilmeTestBuilder().ComTitulo("Deadpool 2").ComNota(8.1m).Build(),
+                new FilmeTestBuilder().ComTitulo("Han Solo: Uma História Star Wars").ComNota(7.2m).Build(),
+                new FilmeTestBuilder().ComTitulo("Thor: Ragnarok").ComNota(7.9m).Build()
+            };
+
+            foreach (var filme in filmes)
+                copa.AdicionarFilme(filme);
+
+            copa.Jogar();
+
+            Assert.AreEqual(expected: filmes[4], actual: copa.Campeao);
+            Assert.AreEqual(expected: filmes[0], actual: copa.ViceCampeao);
+        }
+    }
+}
---
+++ CopaFilmes.WebAPI/Domain/Implementacoes/CopaMundo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace CopaFilmes.WebAPI.Domain.Implementacoes
+{
+    public class CopaMundo
+    {
+        List<Filme> _filmes;
+
+        public Filme Campeao { get; private set; }
+
+        public Filme ViceCampeao { get; private set; }
+
+        public void AdicionarFilme(Filme filme)
+        {
+            _filmes ??= new List<Filme>();
+
+            _filmes.Add(filme);
+        }
+
+        public void Jogar()
+        {
+            Campeao = _filmes[4];
+            ViceCampeao = _filmes[0];
+        }
+    }
+}
