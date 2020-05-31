834b6cfeef58930cf50608944b9d36a490c17e9e
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-05-31 17:30:49 -0300

    adicionado consulta para derrotado de uma partida porque a ideia é obter essa informação da última partida disputada nas eliminatórias (vice-campeão).

--- CopaFilmes.Tests/Domain/Implementacoes/PartidaTests.cs
+++ CopaFilmes.Tests/Domain/Implementacoes/PartidaTests.cs
@@ -9,36 +9,42 @@ namespace CopaFilmes.Tests.Domain.Implementacoes
     public class PartidaTests
     {
         [TestMethod]
-        [DynamicData(nameof(FilmesComNotasDiferentes), DynamicDataSourceType.Property)]
-        public void PartidaTests_Dado_Dois_Filmes_Com_Notas_Diferentes_Quando_Disputarem_Uma_Partida_Retorna_Como_Vencedor_O_Filme_Com_Nota_Maior(decimal notaFilmeA, decimal notaFilmeB, int posicaoVencedor)
+        [DynamicData(nameof(FilmesComNotasDiferentesParaVencedor), DynamicDataSourceType.Property)]
+        public void PartidaTests_Dado_Dois_Filmes_Com_Notas_Diferentes_Quando_Disputarem_Uma_Partida_Retorna_Como_Vencedor_O_Filme_Com_Nota_Maior(decimal notaFilmeA, decimal notaFilmeB, int posicaoVencedor, int posicaoDerrotado)
         {
             var filmeA = new FilmeTestBuilder().ComNota(notaFilmeA).Build();
             var filmeB = new FilmeTestBuilder().ComNota(notaFilmeB).Build();
             var partida = new Partida(filmeA, filmeB);
             var vencedorEsperado = posicaoVencedor == 1 ? filmeA : filmeB;
+            var derrotadoEsperado = posicaoDerrotado == 1 ? filmeA : filmeB;
             var nomeVariavelVencedorEsperado = posicaoVencedor == 1 ? nameof(filmeA) : nameof(filmeB);
+            var nomeVariavelDerrotadoEsperado = posicaoDerrotado == 1 ? nameof(filmeA) : nameof(filmeB);
 
             partida.Disputar();
 
             Assert.AreEqual(expected: vencedorEsperado, actual: partida.Vencedor, 
                 $"O vencedor esperado é a instância de Filme da variável {nomeVariavelVencedorEsperado} porque a nota do Filme A é {notaFilmeA} e a nota do Filme B é {notaFilmeB}.");
+
+            Assert.AreEqual(expected: derrotadoEsperado, actual: partida.Derrotado,
+                $"O derrotado esperado é a instância de Filme da variável {nomeVariavelDerrotadoEsperado} porque a nota do Filme A é {notaFilmeA} e a nota do Filme B é {notaFilmeB}.");
+
         }
 
         /// <summary>
         /// Propriedade utilizada de entrada para cenários de testes.
         /// </summary>
-        public static IEnumerable<object[]> FilmesComNotasDiferentes
+        public static IEnumerable<object[]> FilmesComNotasDiferentesParaVencedor
         {
             get
             {
-                yield return new object[] { 6.7m, 8.5m, 2 };
-                yield return new object[] { 8.1m, 8.8m, 2 };
-                yield return new object[] { 7.2m, 7.9m, 2 };
-                yield return new object[] { 7.8m, 8.5m, 2 };
-                yield return new object[] { 6.7m, 6.3m, 1 };
-                yield return new object[] { 8.8m, 7.9m, 1 };
-                yield return new object[] { 8.5m, 6.7m, 1 };
-                yield return new object[] { 8.8m, 8.5m, 1 };
+                yield return new object[] { 6.7m, 8.5m, 2, 1 };
+                yield return new object[] { 8.1m, 8.8m, 2, 1 };
+                yield return new object[] { 7.2m, 7.9m, 2, 1 };
+                yield return new object[] { 7.8m, 8.5m, 2, 1 };
+                yield return new object[] { 6.7m, 6.3m, 1, 2 };
+                yield return new object[] { 8.8m, 7.9m, 1, 2 };
+                yield return new object[] { 8.5m, 6.7m, 1, 2 };
+                yield return new object[] { 8.8m, 8.5m, 1, 2 };
             }
         }
 
--- CopaFilmes.WebAPI/Domain/Implementacoes/Partida.cs
+++ CopaFilmes.WebAPI/Domain/Implementacoes/Partida.cs
@@ -8,6 +8,8 @@ namespace CopaFilmes.WebAPI.Domain.Implementacoes
 
         internal Filme Vencedor { get; private set; }
 
+        internal Filme Derrotado { get; private set; }
+
         internal Partida(Filme primeiroFilme, Filme segundoFilme)
         {
             _primeiroFilme = primeiroFilme;
@@ -17,13 +19,22 @@ namespace CopaFilmes.WebAPI.Domain.Implementacoes
         internal void Disputar()
         {
             if (_primeiroFilme.PossuiNotaIgual(_segundoFilme))
+            {
                 Vencedor = new Filme[] { _primeiroFilme, _segundoFilme }
                     .OrderBy(f => f)
                     .First();
+            }
             else
-                Vencedor = _primeiroFilme.PossuiNotaMaior(_segundoFilme) 
-                    ? _primeiroFilme 
+            {
+                Vencedor = _primeiroFilme.PossuiNotaMaior(_segundoFilme)
+                    ? _primeiroFilme
                     : _segundoFilme;
+
+                Derrotado = _primeiroFilme.PossuiNotaMaior(_segundoFilme)
+                    ? _segundoFilme
+                    : _primeiroFilme;
+            }
+                
         }
     }
 }
