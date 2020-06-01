97019073da9c05a3e57df9a9d0c9a325f26e9234
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-06-01 20:56:49 -0300

    adicionado teste para criação de endpoint na API que será responsável por obter os filmes enviados pelo front-end e acionar o comportamento de jogar da copa do mundo.

---
+++ CopaFilmes.Tests/Controllers/CopaMundoControllerTests.cs
@@ -0,0 +1,50 @@
+﻿using CopaFilmes.Tests.Domain.Implementacoes.TestBuilders;
+using CopaFilmes.WebAPI.Controllers;
+using CopaFilmes.WebAPI.Domain.Implementacoes;
+using CopaFilmes.WebAPI.Domain.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.Collections.Generic;
+
+namespace CopaFilmes.Tests.Controllers
+{
+    [TestClass]
+    public class CopaMundoControllerTests
+    {
+        [TestMethod]
+        public void CopaMundoControllerTests_Dado_Oito_Ids_De_Filmes_Para_Jogar_A_Copa_Quando_Consumir_Endpoint_Jogar_Retorna_Campeao_E_Vice()
+        {
+            const int OS_INCRIVEIS = 0;
+            const int VINGADORES = 4;
+
+            var filmes = ObterFilmesParaCenarioTestes();
+            var catalogo = Substitute.For<ICatalogoFilmes>();
+            var controller = new CopaMundoController(catalogo);
+            
+            catalogo.ObterPorIds(Arg.Any<List<string>>()).Returns(filmes);
+
+            var resultadoAcao = controller.Get(string.Empty);
+            var resultadoCopa = resultadoAcao.Result.Value;
+
+            catalogo.Received().ObterPorIds(Arg.Any<List<string>>());
+
+            Assert.AreEqual(expected: filmes[VINGADORES], actual: resultadoCopa.Campeao);
+            Assert.AreEqual(expected: filmes[OS_INCRIVEIS], actual: resultadoCopa.ViceCampeao);
+        }
+
+        private List<Filme> ObterFilmesParaCenarioTestes()
+        {
+            return new List<Filme>
+            {
+                new FilmeTestBuilder().ComTitulo("Os Incríveis 2").ComNota(8.5m).Build(),
+                new FilmeTestBuilder().ComTitulo("Jurassic World: Reino Ameaçado").ComNota(6.7m).Build(),
+                new FilmeTestBuilder().ComTitulo("Oito Mulheres e um Segredo").ComNota(6.3m).Build(),
+                new FilmeTestBuilder().ComTitulo("Hereditário").ComNota(7.8m).Build(),
+                new FilmeTestBuilder().ComTitulo("Vingadores: Guerra Infinita").ComNota(8.8m).Build(),
+                new FilmeTestBuilder().ComTitulo("Deadpool 2").ComNota(8.1m).Build(),
+                new FilmeTestBuilder().ComTitulo("Han Solo: Uma História Star Wars").ComNota(7.2m).Build(),
+                new FilmeTestBuilder().ComTitulo("Thor: Ragnarok").ComNota(7.9m).Build()
+            };
+        }
+    }
+}
---
+++ CopaFilmes.WebAPI/Controllers/CopaMundoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CopaFilmes.WebAPI.Domain.Implementacoes;
+using CopaFilmes.WebAPI.Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CopaFilmes.WebAPI.Controllers
+{
+    [ApiController]
+    public class CopaMundoController : ControllerBase
+    {
+        private readonly ICatalogoFilmes _catalogoFilmes;
+
+        public CopaMundoController(ICatalogoFilmes catalogoFilmes) => _catalogoFilmes = catalogoFilmes;
+
+        [HttpGet]
+        [Route("api/[controller]/jogar/{idsFilmesSelecionados}")]
+        public async Task<ActionResult<CopaMundo>> Get(string idsFilmesSelecionados)
+        {
+            var copaMundo = new CopaMundo();
+            var ids = idsFilmesSelecionados.Split(',').ToList();
+            var filmes = await _catalogoFilmes.ObterPorIds(ids);
+
+            foreach (var filme in filmes)
+                copaMundo.Adicionar(filme);
+
+            copaMundo.Jogar();
+            
+            return copaMundo;
+        }
+    }
+}
