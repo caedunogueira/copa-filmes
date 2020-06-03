5ca7849ffdd2fbcc6c0975d07137c5ec382d279e
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-06-03 10:58:29 -0300

    alterado para exibição inicial (esqueleto) da lista de filmes disponíveis a serem exibidas na página.

--- copafilmes-spa/src/app/copamundo.service.ts
+++ copafilmes-spa/src/app/copamundo.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Filme } from './filme';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CopamundoService {
 
-  constructor() { }
+  private enderecoAPI = 'http://localhost:5000/api/copamundo';
+
+  constructor(private http: HttpClient) { }
+
+  obterFilmes(): Observable<Filme[]> {
+    const url = `${this.enderecoAPI}/filmes-disponiveis`;
+    console.log('url requisitada: ' + url);
+    return this.http.get<Filme[]>(url).pipe(
+      catchError(this.handleError<Filme[]>([]))
+    );
+  }
+
+  private handleError<T>(result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      return of(result as T);
+    }
+  }
 }
--- copafilmes-spa/src/app/filmes/filmes.component.ts
+++ copafilmes-spa/src/app/filmes/filmes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Filme } from '../filme';
+import { CopamundoService } from '../copamundo.service';
 
 @Component({
   selector: 'app-filmes',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmesComponent implements OnInit {
 
-  constructor() { }
+  private idsFilmesSelecionados: string[] = [];
+
+  filmes$: Observable<Filme[]>;
+
+  constructor(private copaMundoService: CopamundoService) { }
 
   ngOnInit(): void {
+    this.filmes$ = this.copaMundoService.obterFilmes();
   }
 
+  alterarFilmesSelecionados(id: string, opcao: boolean): void {
+    if (opcao)
+      this.idsFilmesSelecionados.push(id);
+    else {
+      const indice = this.idsFilmesSelecionados.indexOf(id);
+      if (indice !== -1)
+        this.idsFilmesSelecionados.splice(indice, 1);
+    }
+  }
 }
