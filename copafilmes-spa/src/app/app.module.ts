1ae6b69ad41dbeccfa47286b380a184ac851120c
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-06-03 08:06:55 -0300

    adicionado componente para exibição de filmes disponíveis para disputar a copa do mundo.

--- copafilmes-spa/src/app/app.module.ts
+++ copafilmes-spa/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ConteudoComponent } from './conteudo/conteudo.component';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { FilmesComponent } from './filmes/filmes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConteudoComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    FilmesComponent
   ],
   imports: [
     BrowserModule
