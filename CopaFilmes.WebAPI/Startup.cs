2d0c66188a30f3b39c0152a73b669e5f00a416f4
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-06-03 07:40:59 -0300

    removido temporariamente redirecionamento https para não apresentar problemas de certificado quando consumindo a API.

--- CopaFilmes.WebAPI/Startup.cs
+++ CopaFilmes.WebAPI/Startup.cs
@@ -39,8 +39,6 @@ namespace CopaFilmes.WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
