0014739bdfa89d82a74dac2a8029c37513f395a8
Carlos Eduardo Nogueira <carlosedu_nogueira@yahoo.com.br>
2020-06-03 22:34:10 -0300

    adicionado scripts para ajudar na execução das aplicações back-end e front-end, e também script para execução dos testes feitos com uso do MSTest.

---
+++ rodar-front-end.cmd
@@ -0,0 +1,2 @@
+cd copafilmes-spa
+ng serve --prod=true --open=true
---
+++ rodar-testes-back-end.cmd
@@ -0,0 +1,2 @@
+dotnet build --configuration Debug CopaFilmes.Tests
+vstest.console CopaFilmes.Tests\bin\Debug\netcoreapp3.1\CopaFilmes.Tests.dll
